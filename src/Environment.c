3709c25bd2047d140704a3f61e077ba242ef8025
Kayla McArthur <kayla@kayla.is>
2014-08-30 10:39:20 -0700

    Begin implementation of auto-free, add custom alloc/dealloc/stackalloc

--- src/Environment.c
+++ src/Environment.c
@@ -1,10 +1,38 @@
 #include "Environment_Internal.h"
 
+void TR_free(void* ptr)
+{
+      // Remove from our in-use list
+      free(ptr);
+      return;
+}
+
+void* TR_alloc(size_t size)
+{
+      void* result =  malloc(size);
+      
+      // Add to our in-use list
+      
+      return result;
+}
+
+void *TR_stack_alloc(size_t size)
+{
+  if (0) // Switch to using __builtin_frame_address to figure out what frame and can be done safely
+  {
+    // use platform stack allocator if size is 
+  }
+  
+  return TR_alloc(size);
+  
+}
+
 TR_Environment* TR_Environment_alloc()
 {
 	TR_Environment* env =  malloc(sizeof(TR_Environment));
-	env->allocator = &malloc;
-	env->deallocator = &free;
+	env->allocator = &TR_alloc;
+	env->stack_allocator = &TR_stack_alloc;
+	env->deallocator = &TR_free;
 
 	return env;
 }
